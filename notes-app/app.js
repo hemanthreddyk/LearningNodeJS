cc1a1a4366dcdb87895c4a760b34bf17ef4eb344
hemanth <>
2019-11-19 16:27:29 +0530

    notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,52 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
 
-console.log(msg);
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of the Note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+})
 
-const greenMsg = chalk.inverse.bold.green('Success!');
-console.log(greenMsg);
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List note',
+    handler: function () {
+        console.log('Listing out all the notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+})
+
+yargs.parse();
+//console.log(yargs.argv);
